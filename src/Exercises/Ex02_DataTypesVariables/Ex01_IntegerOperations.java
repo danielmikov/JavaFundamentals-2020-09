c056b7d024bc1436199bdab599898253f9e633e9
danielmikov <BKno9jK2TqlDAcnLRFqu>
2020-09-23 10:26:45 +0300

    Exercises.Ex02_DataTypesVariables.Ex01_integerOperations

---
+++ src/Exercises/Ex02_DataTypesVariables/Ex01_IntegerOperations.java
@@ -0,0 +1,16 @@
+package Exercises.Ex02_DataTypesVariables;
+
+import java.util.Scanner;
+
+public class Ex01_IntegerOperations {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int a = Integer.parseInt(scanner.nextLine());
+        int b = Integer.parseInt(scanner.nextLine());
+        int c = Integer.parseInt(scanner.nextLine());
+        int d = Integer.parseInt(scanner.nextLine());
+
+        System.out.println((a+b)/c*d);
+    }
+}
