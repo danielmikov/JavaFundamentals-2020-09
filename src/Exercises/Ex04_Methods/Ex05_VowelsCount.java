04081f02087b1034c63badd6954557eae4000859
danielmikov <BKno9jK2TqlDAcnLRFqu>
2020-10-02 13:24:32 +0300

    Exercises.Ex03_Arrays_MoreExercises.Ex05_VowelsCount

---
+++ src/Exercises/Ex04_Methods/Ex05_VowelsCount.java
@@ -0,0 +1,29 @@
+package Exercises.Ex04_Methods;
+
+import java.util.Scanner;
+
+public class Ex05_VowelsCount {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String inputString = scanner.nextLine();
+        int vowels = 0;
+        for (int i = 0; i < inputString.length(); i++) {
+            char iChar = inputString.charAt(i);
+            vowels = countVowels(vowels, iChar);
+        }
+        System.out.println(vowels);
+    }
+
+    private static int countVowels(int vowels, char iChar) {
+        switch (iChar) {
+            case 'a','A','e','E','i','I','o','O','u','U','y','Y' :
+                vowels++;
+                break;
+            default:
+                break;
+
+        }
+        return vowels;
+    }
+}
